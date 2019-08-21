d5e1634e057ef3616fdfe77d38a10ad679778891
James LeGeros <james@Jamess-MacBook-Air.local>
2019-08-21 16:57:04 -0400

    added token auth with JWT

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://james:semaj2019@devconnector-1d7dk.mongodb.net/test?retryWrites=true&w=majority"
+    "mongodb+srv://james:semaj2019@devconnector-1d7dk.mongodb.net/test?retryWrites=true&w=majority",
+  secretOrKey: "secret"
 };
