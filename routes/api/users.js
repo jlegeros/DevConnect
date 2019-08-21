e2e9aa3941603f8c9f69ec8feb78f71100f20a32
James LeGeros <james@Jamess-MacBook-Air.local>
2019-08-21 17:56:01 -0400

    added passport for authorization

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
+const passport = require("passport");
 
 // Load user model
 const User = require("../../models/User");
@@ -64,7 +65,6 @@ router.post("/login", (req, res) => {
     bcrypt.compare(password, user.password).then(isMatch => {
       if (isMatch) {
         // User Matched
-
         const payload = { id: user.id, name: user.name, avatar: user.avatar }; // Create JWT payload
 
         // Sign token
@@ -85,5 +85,20 @@ router.post("/login", (req, res) => {
     });
   });
 });
+
+// @route   GET api/users/current
+// @desc    Return current user
+// @access  Private
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 
 module.exports = router;
