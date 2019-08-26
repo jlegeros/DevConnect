439a3e0812698ae2450cdf60c0d927f1473a81b6
jlegeros <jlegeros@gmail.com>
2019-08-26 13:20:06 -0400

    added route to create a post

--- models/post.js
+++ models/post.js
@@ -20,7 +20,7 @@ const PostSchema = new Schema({
   likes: [
     {
       user: {
-        type: Schema.Tyoes.ObjectId,
+        type: Schema.Types.ObjectId,
         ref: "users"
       }
     }
@@ -28,7 +28,7 @@ const PostSchema = new Schema({
   comments: [
     {
       user: {
-        type: Schema.Tyoes.ObjectId,
+        type: Schema.Types.ObjectId,
         ref: "users"
       },
       text: {
